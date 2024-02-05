624ad59546006e1fdacfcfeda07a52032ded1e08
Kevin Hong <hong.w.kevin@gmail.com>
2024-02-04 23:11:00 -0800

    add client call to be

--- src/client/src/components/Home/Home.tsx
+++ src/client/src/components/Home/Home.tsx
@@ -1,9 +1,21 @@
 import React, { FC } from 'react';
+import { useHealthCheck } from '../../hooks/useHealthCheck';
 
 interface Props {}
 
 const Home: FC<Props> = () => {
-  return <div>Home</div>;
+  const { data, isLoading } = useHealthCheck();
+
+  if (isLoading) {
+    return <div>Loading</div>;
+  }
+
+  return (
+    <>
+      <div>Home</div>
+      <div>{data.status}</div>
+    </>
+  );
 };
 
 export default Home;
